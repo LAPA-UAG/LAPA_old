fcd1913d54c1dc1ba2e45a17ac115bb6edc55cbc
vinesnts <v.santos0406@gmail.com>
2020-04-03 19:57:39 -0300

    Corrigidas sugestões do Codacy, agora ele ignora arquivos de database pois eles não podem seguir as regras

--- app/Http/Controllers/DisciplinaController.php
+++ app/Http/Controllers/DisciplinaController.php
@@ -7,9 +7,18 @@ use App\Disciplina;
 
 class DisciplinaController extends Controller
 {
+
+    // Model de disciplina adicionado ao controller para evitar uso estatico
+    protected $disciplina;
+
+    public function __construct(Disciplina $disciplina)
+    {
+        $this->disciplina = $disciplina;
+    }
+
     public function index() 
     {
-        $registros = Disciplina::all();
+        $registros = $this->disciplina->all();
         return view('admin.disciplina.index', compact('registros'));
     }
 
@@ -21,28 +30,28 @@ class DisciplinaController extends Controller
     public function salvar(Request $request) 
     {
         $dados = $request->all();
-        Disciplina::create($dados);
+        $this->disciplina->create($dados);
 
         return redirect()->route('admin.disciplinas');
     }
 
     public function editar($id) 
     {
-        $registro = Disciplina::find($id);
+        $registro = $this->disciplina->find($id);
         return view('admin.disciplina.editar', compact('registro'));        
     }
 
     public function atualizar(Request $request, $id)
     {
         $dados = $request->all();
-        Disciplina::find($id)->update($dados);
+        $this->disciplina->find($id)->update($dados);
 
         return redirect()->route('admin.disciplinas');
     }
 
     public function deletar($id)
     {
-        Disciplina::find($id)->delete();
+        $this->disciplina->find($id)->delete();
         return redirect()->route('admin.disciplinas');
     }
 }
